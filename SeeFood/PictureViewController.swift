cc18bd9f72052ccd22008c0ccaafa83225c49d28
Andrey <KonkeyDong37@gmail.com>
2021-01-04 23:18:02 +0500

    Finished the project

---
+++ SeeFood/PictureViewController.swift
@@ -0,0 +1,79 @@
+//
+//  PictureViewController.swift
+//  SeeFood
+//
+//  Created by Андрей on 04.01.2021.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class PictureViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    var userImage: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = ""
+        self.imageView.layer.cornerRadius = self.imageView.frame.width / 2
+        self.imageView.layer.borderColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        self.imageView.layer.borderWidth = 10
+        setImage()
+    }
+    
+    func setImage() {
+        guard let userImage = self.userImage else { return }
+        imageView.image = userImage
+        
+        guard let ciImage = CIImage(image: userImage) else {
+            fatalError("Could not convert to CIImage")
+        }
+        
+        detect(image: ciImage)
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model: VNCoreMLModel = {
+            do {
+                let config = MLModelConfiguration()
+                return try VNCoreMLModel(for: Inceptionv3(configuration: config).model)
+            }
+            catch {
+                print(error)
+                fatalError("Couldn't create SleepCalculator")
+            }
+        }() else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            if let firstResult = result.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.titleLabel.text = "Hotdog!"
+                    self.imageView.layer.borderColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+                } else {
+                    self.titleLabel.text = "Not Hotdog!"
+                    self.imageView.layer.borderColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+                }
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+}
--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let imagePicker = UIImagePickerController()
+    var pictureVC: PictureViewController?
+    var selectedPicture: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pictureVC = PictureViewController()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let userPickedimage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        
+        selectedPicture = userPickedimage
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        performSegue(withIdentifier: "pictureViewSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let dvc = segue.destination as? PictureViewController else { return }
+        dvc.userImage = selectedPicture
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIButton) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
